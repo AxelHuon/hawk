d063e770668dd63f00431d41a94be4d15728b59d
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-03-19 15:51:17 +0100

    [feature/instance-fetch][feat]:add axios instance

--- src/data/users/users.ts
+++ src/data/users/users.ts
@@ -1,9 +1,37 @@
 export const users: User[] = [
   {
-    id: 'usr-ccf70265ef1ed0788ede14b5722ce76b1c589e3408a48edd79efa400c54dd5b3',
+    id: 'usr-ccded5a8-0724-4377-a02c-c793db92043e',
     username: 'User',
     password: 'password',
     email: 'root@root.fr',
     role: 'MJ',
+  },
+  {
+    id: 'usr-deaa7648-b131-4ddc-a7de-a32609b87f8c',
+    username: 'Toto',
+    password: 'password',
+    email: 'toto@toto.fr',
+    role: 'Users',
+  },
+  {
+    id: 'usr-ed57689b-51c1-4349-9108-4b8618ff337f',
+    username: 'Titi',
+    password: 'password',
+    email: 'titi@titi.fr',
+    role: 'Users',
+  },
+  {
+    id: 'usr-67d24f63-092c-4920-b7fa-376ae2c3d6cd',
+    username: 'Tata',
+    password: 'password',
+    email: 'tata@tata.fr',
+    role: 'Users',
+  },
+  {
+    id: 'usr-a9544e98-62e5-4a00-95b5-6cb64d33856b',
+    username: 'root',
+    password: 'password',
+    email: 'root@root.fr',
+    role: 'Admin',
   },
 ];

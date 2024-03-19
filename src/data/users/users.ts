2e1355f14feafb0a76c06bfae64840bca2748cfc
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-03-19 12:03:29 +0100

    [feature/add-new-docs][feat]:add new docs

--- src/data/users/users.ts
+++ src/data/users/users.ts
@@ -1,6 +1,6 @@
 export const users: User[] = [
   {
-    id: 'ccf70265ef1ed0788ede14b5722ce76b1c589e3408a48edd79efa400c54dd5b3',
+    id: 'usr-ccf70265ef1ed0788ede14b5722ce76b1c589e3408a48edd79efa400c54dd5b3',
     username: 'User',
     password: 'password',
     email: 'root@root.fr',
